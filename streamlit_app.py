03c9ea1867ca4418ac2e8d6f7c32be39debcce4d
Nikki <nikkicci79@gmail.com>
2025-01-26 19:13:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ input_row = df_penguins[:1] # first row only
 target_mapper = {'Adelie': 0,
                 'Chinstrap': 1,
                 'Gentoo': 2}
-def target_encode(val)
+def target_encode(val):
   return target_mapper[val]
 
 with st.expander('Input features'):
