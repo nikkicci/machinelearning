9b7b39b4f30ab46bd4ab9343d64ebafbe156c464
Nikki <nikkicci79@gmail.com>
2025-01-26 18:25:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,17 +29,21 @@ with st.sidebar:
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0) # min value, max value, average value
   gender = st.selectbox('Gender', ('male', 'female'))
   
-# Create a DataFrame for the input features
-data = {'island': island,
+  # Create a DataFrame for the input features
+  data = {'island': island,
        'bill_length_mm': bill_length_mm,
        'bill_depth_mm': bill_depth_mm,
       'flipper_length_mm': flipper_length_mm,
       'body_mass_g': body_mass_g,
       'gender': gender}
-input_df = pd.DataFrame(data, index=[0])
-input_penguins = pd.concat([input_df, x], axis=0)
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, x], axis=0)
 
-input_penguins
+with st.expander('Input features'):
+  st.write('**Input penguin**')
+  input_df
+  st.write('**Combined penguins data**')
+  input_penguins
 
 
 
