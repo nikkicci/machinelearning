edb638394ede723a934245b63146c347521398f9
Nikki <nikkicci79@gmail.com>
2025-01-26 18:17:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,8 @@ data = {'island': island,
 input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, x], axis=0)
 
-input_df
-# input_penguins
+# input_df
+input_penguins
+
+
 
