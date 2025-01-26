e7cfaeb1a8bd44dd62fd726d4113e0f1e7cf04ce
Nikki <nikkicci79@gmail.com>
2025-01-26 15:06:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('🪄 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
